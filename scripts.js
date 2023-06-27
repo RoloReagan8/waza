daa0c5ae6b6a60554961c26f031d2321ecfff8c6
RoloReagan8 <137957440+RoloReagan8@users.noreply.github.com>
2023-06-28 00:22:56 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ var myImage = document.getElementById('myImage');
 // Add a click event listener to the button
 showImageBtn.addEventListener('click', function() {
   // Set the image source
-  myImage.src = 'quim1.jpg';
+  myImage.src = 'quim1.jpeg';
   
   // Show the image container
   imageContainer.style.display = 'block';
