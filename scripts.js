328638865c9334bbaaec2482f073410be4e7143c
RoloReagan8 <137957440+RoloReagan8@users.noreply.github.com>
2023-06-28 19:21:13 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var counterValue = 0;
-  var counterElement = document.getElementById('counterValue');
 
-  setInterval(function() {
-    counterValue++;
-    counterElement.textContent = counterValue;
-  }, 1000);
-});
